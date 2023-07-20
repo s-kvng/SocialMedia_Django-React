a0d65729cf10af2116932251851c0f885026556b
nathan <kvngnathan8420@gmail.com>
2023-07-20 14:20:50 +0000

    add core_user to settings

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "core.user",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# tell django to use the User in user app for authentication
+AUTH_USER_MODEL = "core_user.User"
