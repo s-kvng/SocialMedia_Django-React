54870edf7b18d7fde76ceda450b3a0a91b866bcb
nathan <kvngnathan8420@gmail.com>
2023-08-05 15:18:51 +0000

    create endpoint to  register users

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -135,7 +135,7 @@ AUTH_USER_MODEL = "core_user.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_FILTER_BACKENDS": ["django.filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
