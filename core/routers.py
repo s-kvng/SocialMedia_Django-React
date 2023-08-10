a8946d51bb131a31364c1d74134fe3f29b7a03a7
s-kvng <kvngnathan8420@gmail.com>
2023-08-10 00:02:59 +0000

    login feature done

--- core/routers.py
+++ core/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 
 
 """_summary_
@@ -15,6 +15,7 @@ router = routers.SimpleRouter()
 # ##################################################################### #
 
 router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
 
 router.register(r"user", UserViewSet, basename="user")
 
