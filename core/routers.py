d9efc061ac090a1f72f3aec9e2ea37f0ac18b0fa
s-kvng <kvngnathan8420@gmail.com>
2023-08-10 00:51:40 +0000

    add refresh logic

--- core/routers.py
+++ core/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet, LoginViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 
 """_summary_
@@ -16,6 +16,7 @@ router = routers.SimpleRouter()
 
 router.register(r"auth/register", RegisterViewSet, basename="auth-register")
 router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
 
 router.register(r"user", UserViewSet, basename="user")
 
