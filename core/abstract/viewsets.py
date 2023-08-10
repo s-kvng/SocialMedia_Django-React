8958c98187735c3504ed4bc0929aefac3ddf0993
s-kvng <kvngnathan8420@gmail.com>
2023-08-10 23:21:05 +0000

    add abstract viewsets

---
+++ core/abstract/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from rest_framework import filters
+
+
+class AbstractViewSet(viewsets.ModelViewSet):
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["update_at", "created_at"]
+    ordering = ["updated_at"]
