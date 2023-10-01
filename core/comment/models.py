f879c7ff9c9388d09816a0bfbe51641a77d2102b
s-kvng <kvngnathan8420@gmail.com>
2023-10-01 11:54:29 +0000

    write comment model

--- core/comment/models.py
+++ core/comment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from core.abstract.models import AbstractModel , AbstractManager 
+
 # Create your models here.
+
+class CommentManager(AbstractManager):
+    pass
+
+class Comment(AbstractModel):
+    post = models.ForeignKey('core_post.Post', related_name='', on_delete=models.PROTECT)
+    author = models.ForeignKey('core_user.User', related_name='posted_by', on_delete=models.PROTECT)
+    body = models.TextField()
+    edited = models.BooleanField(default=False)
+
+    objects = CommentManager()
+
+    def __str__(self):
+        return f"{self.author.name}"
