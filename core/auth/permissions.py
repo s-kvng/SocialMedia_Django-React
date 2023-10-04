a38a8d6ca29616be43617c30a233e5165f3ba50c
s-kvng <kvngnathan8420@gmail.com>
2023-10-04 15:56:06 +0000

    write custom permissions

---
+++ core/auth/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import BasePermission , SAFE_METHODS
+
+class UserPermission(BasePermission):
+    ''' This method checks permission based on specific objects'''
+    def has_object_permission(self , request , view, obj):
+        if request.user.is_anonymous:
+            return request.method in SAFE_METHODS
+        
+        if view.basename in ["post"]:
+            return bool(request.user and request.user.is_authenticated)
+        
+        if view.basename in ["post-comment"]:
+            if request.method in ["DELETE"]:
+                return bool(request.is_superuser or request.user in [obj.author , obj.post.author])
+            return bool(request.user and request.user.is_authenticated)
+        
+        return False
+    
+    ''' This method checks permission on a view level'''
+    def has_permission(self , request , view):
+        if view.basename in ["post", "user" , "post-comment"]:
+            if request.user.is_anonymous:
+                return request.method in SAFE_METHODS
+            return bool(request.user and request.user.is_authenticated)
+        
+        return False
