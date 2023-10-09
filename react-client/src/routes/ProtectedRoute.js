663fa73d65b3e4d120696a749ab23f8f3617b053
s-kvng <kvngnathan8420@gmail.com>
2023-10-09 17:32:53 +0000

    route fixes

--- react-client/src/routes/ProtectedRoute.js
+++ react-client/src/routes/ProtectedRoute.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 import { Navigate } from "react-router-dom";
+import { getUser } from "../hooks/user.action";
 const ProtectedRoute = ({ children }) => {
-  const { user } = JSON.parse(localStorage.getItem("auth"));
+  const user = getUser();
 
   return user ? <>{children}</> : <Navigate to={"/login/"} />;
 };
