6a4b6944dcd92312b4eada0264277132fb4a307c
s-kvng <kvngnathan8420@gmail.com>
2023-10-09 16:37:04 +0000

    update login page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,20 +4,21 @@ import { Route, Routes } from "react-router-dom";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route
-        to="/"
+        path="/"
         element={
           <ProtectedRoute>
             <Home />
           </ProtectedRoute>
         }
       />
-      <Route path="/login/" element={<div>Login</div>} />
+      <Route path="/login/" element={<Login />} />
     </Routes>
   );
 }
--- react-client/src/routes/ProtectedRoute.js
+++ react-client/src/routes/ProtectedRoute.js
@@ -3,7 +3,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { user } = JSON.parse(localStorage.getItem("auth"));
-  return user ? <div>{children}</div> : <Navigate to={"/login/"} />;
+
+  return user ? <>{children}</> : <Navigate to={"/login/"} />;
 };
 
 export default ProtectedRoute;
