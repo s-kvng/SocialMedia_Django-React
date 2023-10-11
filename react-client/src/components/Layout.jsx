1c7f4228b196fd66b904c64258a85f5743f93bbd
s-kvng <kvngnathan8420@gmail.com>
2023-10-11 01:31:53 +0000

    create layout

---
+++ react-client/src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavigationBar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <NavigationBar />
+      <div className="container m-5">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
